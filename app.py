da5538ed30cf32c00f65dbcfa334c01ef441bfbe
Fakhruddin Bharmal <fakhruddin.b@milestoneinternet.com>
2020-09-29 18:15:46 +0530

    Testing Commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_restful import Resource, Api, reqparse
 
 # Create an instance of Flask
 app = Flask(__name__)
-#app.config["DEBUG"] = True
+app.config["DEBUG"] = True
 # Create the API
 api = Api(app)
 
@@ -37,6 +37,37 @@ class GetFilling(Resource):
             return {'message': 'Record not found', 'data': {}}, 404
 
         return {'message': 'Record found', 'data': shelf[unique_id]}, 200
-    
+
+class Create(Resource):
     def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('unique_id', required=True)
+        parser.add_argument('link')
+        parser.add_argument('user')
+        parser.add_argument('stockCodes')
+        parser.add_argument('row_id')
+        parser.add_argument('order_id')
+        parser.add_argument('created_date')
+        # Parse the arguments into an object
+        args = parser.parse_args()
+        shelf = get_db()
+        shelf[args['unique_id']] = args
+
+        return {'message': 'Record Created', 'data': args}, 201 
+
+class AllFilingList(Resource):
+    def get(self):
+        shelf = get_db()
+        keys = list(shelf.keys())
+
+        devices = []
+
+        for key in keys:
+            devices.append(shelf[key])
+
+        return {'message': 'Success', 'data': devices}, 200
+
+api.add_resource(Create, '/createfilling')  
 api.add_resource(GetFilling, '/filing/<string:unique_id>')
+api.add_resource(AllFilingList, '/allfiling')
+app.run()
