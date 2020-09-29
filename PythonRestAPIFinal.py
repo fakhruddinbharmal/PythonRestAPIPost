b55342f112a0db3c13b55f71bc5123e72fe128d9
Fakhruddin Bharmal <bharmalfakhruddin@gmail.com>
2020-09-29 11:40:31 +0530

    Update PythonRestAPIFinal.py

--- PythonRestAPIFinal.py
+++ PythonRestAPIFinal.py
@@ -3,16 +3,16 @@ import os
 
 # Import the framework
 from flask import Flask, g
-from flask_restful import Resource, Api, reqparse
+#from flask_restful import Resource, Api, reqparse
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Create the API
-api = Api(app)
+#api = Api(app)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/',methods=['get'])
 return "hello"
-
+app.run
